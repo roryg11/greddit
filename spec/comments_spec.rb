a4ddcd7063b2eefa2ff58e1d2b3680bcbe638922
roryg11 <Rory.C.Grant@gmail.com>
2014-12-27 18:53:19 -0700

    making CRUD tests pass with sign-in

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -1,48 +1,99 @@
 require 'rails_helper'
 
 feature 'CRUDing comments' do
-  scenario 'user can add a comment on a post show page' do
+  scenario 'a signed in user can add a comment on a post show page' do
+    user = User.create!(
+    username: "Bob",
+    email: "Bob@Bob.com",
+    password: "bob",
+    password_confirmation: "bob"
+    )
+
     post = Post.create!(
       title: "gReddit is so cool",
-      content: "gReddit is a pretty good place to practice Ruby on Rails"
+      content: "gReddit is a pretty good place to practice Ruby on Rails",
+      user_id: "#{user.id}"
     )
 
     visit root_path
+    #sign in action
+    click_on 'sign-in-form'
+    fill_in "E-mail", with: "Bob@Bob.com"
+    fill_in "Password", with: "bob"
+    click_on "signin-submit-action"
+
+    #add comment action
     click_on "show-post-#{post.id}-action"
     click_on "add-comment-to-post-#{post.id}-action"
     fill_in "Comment", with: "I'm not sure. I think I like Reddit better."
     click_on 'submit-comment-action'
 
     expect(page).to have_content("Comment succcessfully added.")
+    expect(page).to have_content("posted by Bob on #{post.created_at.strftime("%b %d, %Y")}")
   end
-  scenario 'user can edit a comment on a post show page' do
+
+  scenario 'a signed in user can edit a comment on a post show page' do
+    user = User.create!(
+    username: "Bob",
+    email: "Bob@Bob.com",
+    password: "bob",
+    password_confirmation: "bob"
+    )
+
     post = Post.create!(
     title: "gReddit is so cool",
-    content: "gReddit is a pretty good place to practice Ruby on Rails"
+    content: "gReddit is a pretty good place to practice Ruby on Rails",
+    user_id: "#{user.id}"
     )
 
     comment = Comment.create!(
       content: "I'm not sure. I think I like Reddit better.",
-      post_id: (post.id)
+      post_id: "#{post.id}",
+      user_id: "#{user.id}"
     )
 
+    #sign in action
+    visit root_path
+    click_on 'sign-in-form'
+    fill_in "E-mail", with: "Bob@Bob.com"
+    fill_in "Password", with: "bob"
+    click_on "signin-submit-action"
+
+    #edit comment action
     visit post_path(post)
     click_on "edit-comment-#{comment.id}-action"
     fill_in "Comment", with: "Actually I totally agree."
     click_on "submit-comment-action"
 
     expect(page).to have_content("Comment successfully updated.")
+    expect(page).to have_content("Actually I totally agree.")
   end
   scenario 'user can delete a comment on a post show page' do
+    user = User.create!(
+    username: "Bob",
+    email: "Bob@Bob.com",
+    password: "bob",
+    password_confirmation: "bob"
+    )
+
     post = Post.create!(
     title: "gReddit is so cool",
-    content: "gReddit is a pretty good place to practice Ruby on Rails"
+    content: "gReddit is a pretty good place to practice Ruby on Rails",
+    user_id: "#{user.id}"
     )
 
     comment = Comment.create!(
     content: "I'm not sure. I think I like Reddit better.",
-    post_id: (post.id)
+    post_id: "#{post.id}",
+    user_id: "#{user.id}"
     )
+
+    #sign in action
+    visit root_path
+    click_on 'sign-in-form'
+    fill_in "E-mail", with: "Bob@Bob.com"
+    fill_in "Password", with: "bob"
+    click_on "signin-submit-action"
 
     visit post_path(post)
     click_on "delete-comment-#{comment.id}-action"
@@ -50,24 +101,45 @@ feature 'CRUDing comments' do
     expect(page).to have_content("Comment was successfully deleted.")
   end
   scenario 'user can see all comments on the post show page' do
+    user = User.create!(
+    username: "Bob",
+    email: "Bob@Bob.com",
+    password: "bob",
+    password_confirmation: "bob"
+    )
+
     post = Post.create!(
     title: "gReddit is so cool",
-    content: "gReddit is a pretty good place to practice Ruby on Rails"
+    content: "gReddit is a pretty good place to practice Ruby on Rails",
+    user_id: "#{user.id}"
     )
 
     comment = Comment.create!(
     content: "I'm not sure. I think I like Reddit better.",
-    post_id: (post.id)
+    post_id: "#{post.id}",
+    user_id: "#{user.id}"
     )
 
-    comment = Comment.create!(
+    comment_2 = Comment.create!(
     content: "Nah this one is awesome.",
-    post_id: (post.id)
+    post_id: "#{post.id}",
+    user_id: "#{user.id}"
     )
 
     visit post_path(post)
 
     expect(page).to have_content("I'm not sure. I think I like Reddit better.")
+    expect(page).to have_content("posted by Bob on #{post.created_at.strftime("%b %d, %Y at %H:%M")}")
     expect(page).to have_content("Nah this one is awesome.")
   end
+end
+
+feature "When user is not signed in" do
+  scenario 'User cannot add comments'
+  scenario 'User cannot edit or delete comments'
+end
+
+
+feature 'User can only edit their own content' do
+  scenario 'User cannot edit or delete comments belonging to anther user'
 end
--- spec/posts_spec.rb
+++ spec/posts_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 feature 'CRUD posts' do
-  scenario 'Write a new post' do
+  scenario 'A Signed in User can Write a new post' do
+    user = User.create!(
+      username: "Bob",
+      email: "Bob@Bob.com",
+      password: "bob",
+      password_confirmation: "bob"
+    )
+
     visit root_path
+    click_on 'sign-in-form'
+    fill_in "E-mail", with: "Bob@Bob.com"
+    fill_in "Password", with: "bob"
+    click_on "signin-submit-action"
     click_on 'new-post-action'
     fill_in "Title", with: "I love gReddit"
     fill_in "Content", with: "It's so much better than Reddit"
@@ -10,16 +21,31 @@ feature 'CRUD posts' do
 
     expect(page).to have_content("I love gReddit")
     expect(page).to have_content("It's so much better than Reddit")
+    expect(page).to have_content("posted by Bob")
   end
 
-  scenario 'edit a post' do
-    post = Post.create!(
-      title: "I love gReddit",
-      content: "It's so much better than Reddit"
+  scenario 'A signed in user can edit a post' do
+    user = User.create!(
+    username: "Bob",
+    email: "Bob@Bob.com",
+    password: "bob",
+    password_confirmation: "bob"
     )
 
+    post = Post.create!(
+      title: "I love gReddit",
+      content: "It's so much better than Reddit",
+      user_id: "#{user.id}"
+    )
 
     visit root_path
+    #signin action
+    click_on 'sign-in-form'
+    fill_in "E-mail", with: "Bob@Bob.com"
+    fill_in "Password", with: "bob"
+    click_on "signin-submit-action"
+
+    #edit post action
     click_on "edit-post-#{post.id}-action"
     fill_in "Title", with: "I hate gReddit"
     fill_in "Content", with: "It sucks"
@@ -29,43 +55,87 @@ feature 'CRUD posts' do
     expect(page).to have_content("It sucks")
   end
 
-  scenario 'delete a post' do
-    post = Post.create!(
-    title: "I love gReddit",
-    content: "It's so much better than Reddit"
+  scenario 'a signed in user can delete their own post' do
+    user = User.create!(
+    username: "Bob",
+    email: "Bob@Bob.com",
+    password: "bob",
+    password_confirmation: "bob"
     )
 
+    post = Post.create!(
+    title: "I love gReddit",
+    content: "It's so much better than Reddit",
+    user_id: "#{user.id}"
+    )
+
+
     visit root_path
+    #signin action
+    click_on 'sign-in-form'
+    fill_in "E-mail", with: "Bob@Bob.com"
+    fill_in "Password", with: "bob"
+    click_on "signin-submit-action"
     click_on "delete-post-#{post.id}-action"
 
     expect(page).to have_content("Post successfully deleted.")
   end
-  scenario 'view an individual post' do
+
+  scenario 'any user can view an individual post without signing in' do
+    user = User.create!(
+    username: "Bob",
+    email: "Bob@Bob.com",
+    password: "bob",
+    password_confirmation: "bob"
+    )
+
     post = Post.create!(
     title: "I love gReddit",
-    content: "It's so much better than Reddit"
+    content: "It's so much better than Reddit",
+    user_id: "#{user.id}"
     )
 
     visit root_path
     click_on "show-post-#{post.id}-action"
 
     expect(page).to have_content("I love gReddit")
+    expect(page).to have_content("Sign In")
+    expect(page).to have_content("Sign Up")
   end
 
   scenario 'view an index of posts' do
+    user = User.create!(
+    username: "Bob",
+    email: "Bob@Bob.com",
+    password: "bob",
+    password_confirmation: "bob"
+    )
+
     Post.create!(
     title: "I love gReddit",
-    content: "It's so much better than Reddit"
+    content: "It's so much better than Reddit",
+    user_id: "#{user.id}"
     )
 
     Post.create!(
     title: "Reddit sucks",
-    content: "Reddit is just a stream of silly information."
+    content: "Reddit is just a stream of silly information.",
+    user_id: "#{user.id}"
     )
 
     visit root_path
 
     expect(page).to have_content("Reddit sucks")
     expect(page).to have_content("I love gReddit")
+    expect(page).to have_content("posted by Bob")
   end
+end
+
+feature 'When a user is not signed in' do
+  scenario 'User cannot create a post'
+  scenario 'User cannot edit or delete posts'
+end
+
+feature 'User can only edit their own content' do
+  scenario 'User cannot edit or delete posts belonging to anther user'
 end
